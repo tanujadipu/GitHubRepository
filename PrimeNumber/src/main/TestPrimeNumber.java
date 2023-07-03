2b5b4ecd28d76f8354a47d180e739392cf6cc7e4
Tanuja Patnaik <tanuja.patnaik20010@gmail.com>
2023-07-03 15:00:16 +0530

    My First Commit to Git Hub Project

---
+++ PrimeNumber/src/main/TestPrimeNumber.java
@@ -0,0 +1,23 @@
+package main;
+
+public class TestPrimeNumber {
+
+	public static void main(String[] args) {
+		int i = 6;
+		int counter = 0;
+		for(int j = 2 ; j<=4 ;j++) {
+			if(i%j == 0) {
+				counter = counter + 1;
+			}
+		}
+		if(counter == 0) {
+			System.out.println(i+ ": is a prime number ");
+		}
+		else {
+			System.out.println(i+ ": is not a prime number ");
+		}
+		
+
+	}
+
+}
---
+++ PrimeNumber/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author tpatnaik
+ *
+ */
+module PrimeNumber {
+}
