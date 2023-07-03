4e6cd0f47e2a9cc0b5dbcd417a579bbd0d180c1e
Tanuja Patnaik <tanuja.patnaik20010@gmail.com>
2023-07-03 15:15:38 +0530

    First Commit to Local Branch

--- PrimeNumber/src/main/TestPrimeNumber.java
+++ PrimeNumber/src/main/TestPrimeNumber.java
@@ -3,7 +3,7 @@ package main;
 public class TestPrimeNumber {
 
 	public static void main(String[] args) {
-		int i = 6;
+		int i = 10;
 		int counter = 0;
 		for(int j = 2 ; j<=4 ;j++) {
 			if(i%j == 0) {
