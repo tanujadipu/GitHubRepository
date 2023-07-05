3e9c450a3439046023901032a0633e84f5f282df
Tanuja Patnaik <tanuja.patnaik20010@gmail.com>
2023-07-05 16:09:45 +0530

    Local one setp back from master

--- PrimeNumber/src/main/TestOddNumber.java
+++ PrimeNumber/src/main/TestOddNumber.java
@@ -3,7 +3,8 @@ package main;
 public class TestOddNumber {
 
 	public static void main(String[] args) {
-		int i = 5;
+		int i = 8;
+		// Making even with Local one step back from Main
 		if(i%2 == 0) {
 			System.out.println(i+ " : is a even number ");
 		}
