f859492eeba8ce655386880cbf4f757444a779c3
Tanuja Patnaik <tanuja.patnaik20010@gmail.com>
2023-07-05 16:47:36 +0530

    test

--- PrimeNumber/src/main/Multiples.java
+++ PrimeNumber/src/main/Multiples.java
@@ -3,7 +3,7 @@ package main;
 public class Multiples {
 
 	public static void main(String[] args) {
-		int i = 6;
+		int i = 69;
 		for(int j = 1 ; j<=10 ;j++) 
 		{
 			System.out.println(i+"*"+j+"="+ i*j);
